b822186f615309be58c8f21f3dba74ec82a6f228
Jaspreet <jaspreetkn92@gmail.com>
2025-02-18 12:52:07 +0000

    created the footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, FooterComponent],
   template: `
     <main>
       <app-navbar></app-navbar>
       <app-hero></app-hero>
+      <app-footer></app-footer>
     </main>
   `,
 
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `
+    <footer class="text-white p-6">
+      <div class="grid grid-cols-2">
+        <div>
+          <ul>
+            <li>
+              <a href="/" class="hover:text-red-300 transition-all">Home</a>
+            </li>
+            <li>
+              <a href="/" class="hover:text-red-300 transition-all">Product</a>
+            </li>
+            <li>
+              <a href="/" class="hover:text-red-300 transition-all">About us</a>
+            </li>
+            <li>
+              <a href="/" class="hover:text-red-300 transition-all">Blog</a>
+            </li>
+            <li>
+              <a href="/" class="hover:text-red-300 transition-all"
+                >Contact us</a
+              >
+            </li>
+          </ul>
+        </div>
+
+        <div class="flex-col">
+          <ul>
+            <li><a href="/" class="underline">Privacy Policy</a></li>
+          </ul>
+
+          <div class="flex gap-4">
+            <p>Email</p>
+            <p>Linkedin</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="md:flex md:items-center md:justify-between">
+        <div>
+          <p class="pt-6 font-light">
+            &copy; Clearly 2025, Together Global Ltd. trading as "Clearly"
+          </p>
+          <p class="font-light text-gray-300 pb-4">
+            EIT Urban Mobility is supported by the European Institute of
+            Innovation and Technology (EIT), a body of the European Union
+          </p>
+        </div>
+
+        <span
+          >Website:
+          <a
+            class="text-zinc-500 underline"
+            href="https://togethercreative.co.uk/"
+            >Together</a
+          ></span
+        >
+      </div>
+    </footer>
+  `,
+})
+export class FooterComponent {}
