bf53e03b6c8eb20f2c1c28bbc22a2ccca1740e74
Jaspreet <jaspreetkn92@gmail.com>
2025-02-22 12:05:40 +0000

    added mobile navbar content

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -44,8 +44,48 @@ import { RouterModule } from '@angular/router';
           </li>
         </ul>
       </div>
+
       <app-button label="Menu" class="md:hidden"></app-button>
     </nav>
+
+    <!-- Mobile Navbar -->
+
+    <div class="p-6">
+      <ul class=" text-white font-bold text-xl flex flex-col gap-6">
+        <li>
+          <a href="#" class="hover:text-red-300 transition-all">Product</a>
+        </li>
+
+        <li>
+          <a
+            routerLink="/about"
+            routerLinkActive="text-red-300"
+            class="hover:text-red-300 transition-all"
+            >About Us</a
+          >
+        </li>
+
+        <li>
+          <a
+            routerLink="/blog"
+            routerLinkActive="text-red-300"
+            class="hover:text-red-300 transition-all"
+            >Blog</a
+          >
+        </li>
+        <li>
+          <a
+            routerLink="/contact"
+            routerLinkActive="text-red-300"
+            class="hover:text-red-300 transition-all"
+            >Contact Us</a
+          >
+        </li>
+      </ul>
+      <a routerLink="/contact">
+        <app-button label="Request a Demo" customClass="mt-24"></app-button>
+      </a>
+    </div>
   `,
 })
 export class NavbarComponent {}
