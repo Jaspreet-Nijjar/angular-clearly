e9487bce20cd64bd305af8035687ffa716ace24d
Jaspreet <jaspreetkn92@gmail.com>
2025-02-23 12:47:38 +0000

    added the investors section

--- src/app/components/feature-section/feature-section.component.ts
+++ src/app/components/feature-section/feature-section.component.ts
@@ -5,17 +5,23 @@ import { CommonModule } from '@angular/common';
   selector: 'app-feature-section',
   imports: [CommonModule],
   template: `
-    <section [ngClass]="customClass" class="text-white p-4 relative h-60">
+    <section [ngClass]="customClass" class="text-white p-4 relative ">
       <div class="bg-white w-8 h-2"></div>
       <h3
         class="absolute w-44 top-8 left-[-40px] p-6 rotate-270 font-semibold text-xl"
       >
         {{ label }}
       </h3>
+      <div class="px-24 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        <div *ngFor="let item of items">
+          <img [src]="item.icon" alt="company icon" class="w-45 mt-4 mb-4" />
+        </div>
+      </div>
     </section>
   `,
 })
 export class FeatureSectionComponent {
   @Input() label: string = '';
   @Input() customClass: string = '';
+  @Input() items: { icon: string }[] = [];
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,8 +11,18 @@ import { FeatureSectionComponent } from '../../components/feature-section/featur
       <app-feature-section
         customClass="bg-zinc-400"
         label="Investors & Partners"
+        [items]="investors"
       ></app-feature-section>
     </main>
   `,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  investors = [
+    { icon: '/investors/pace-centures.png' },
+    { icon: '/investors/EU_Logo.png' },
+    { icon: '/investors/mobilion.png' },
+    { icon: '/investors/NextGear-WHITE.png' },
+    { icon: '/investors/nine_realms-greyscale.png' },
+    { icon: '/investors/m1720-greyscale.png' },
+  ];
+}
