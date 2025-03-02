f1860bfa3a4374d5d4170f60cbad33542af6f559
Jaspreet <jaspreetkn92@gmail.com>
2025-03-02 15:55:18 +0000

    finalising style changes to form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -5,17 +5,17 @@ import { ButtonComponent } from '../../button/button.component';
   selector: 'app-contact',
   imports: [ButtonComponent],
   template: `
-    <h1 class="text-4xl font-bold text-white">Contact us</h1>
+    <h1 class="text-4xl font-bold text-white p-6">Contact us</h1>
     <section class="p-6 flex flex-col text-white md:flex-row md:gap-4">
-      <form class="flex flex-col md:flex-row md:gap-54">
+      <form class="flex flex-col md:flex-row md:gap-54 lg:gap-60">
         <div class="flex flex-col space-y-6">
-          <div class="mb-2">
+          <div>
             <label
               >Select your interest <span class="text-red-500">*</span></label
             >
 
             <select
-              class="text-white bg-black border border-white px-4 py-2 rounded block w-[300px]"
+              class="text-white bg-black border mb-2 border-white px-4 py-2 rounded block w-[300px] lg:w-[500px]"
             >
               <option value="" disabled selected>
                 Select from dropdown items
@@ -42,52 +42,52 @@ import { ButtonComponent } from '../../button/button.component';
             </select>
           </div>
 
-          <div class="mb-2">
+          <div>
             <label>Subject of query<span class="text-red-500">*</span></label>
             <input
               type="text"
               placeholder="Type..."
-              class="block text-white bg-black border border-white px-4 py-2 rounded w-[300px]"
+              class="block text-white bg-black border border-white mb-2 px-4 py-2 rounded w-[300px] lg:w-[500px]"
             />
           </div>
 
-          <div class="mb-2">
+          <div>
             <label>Your message<span class="text-red-500">*</span></label>
             <textarea
               rows="5"
               placeholder="Type..."
-              class="block text-white bg-black border border-white px-4 py-2 rounded w-[300px]"
+              class="block text-white bg-black border border-white px-4 py-2 mb-2 rounded w-[300px] lg:w-[500px]"
             ></textarea>
           </div>
         </div>
 
-        <div class="flex flex-col gap-2 space-y-4 md:w-1/2">
+        <div class="flex flex-col gap-2 space-y-4">
           <label>First name<span class="text-red-500">*</span></label>
           <input
             type="text"
             placeholder="Type..."
-            class="block text-white bg-black border border-white px-4 py-2 rounded w-[300px]"
+            class="block text-white bg-black border border-white mb-2 px-4 py-2 rounded w-[300px] lg:w-[500px]"
           />
 
           <label>Last name<span class="text-red-500">*</span></label>
           <input
             type="text"
             placeholder="Type..."
-            class="block text-white bg-black border border-white px-4 py-2 rounded w-[300px]"
+            class="block text-white bg-black border border-white mb-2 px-4 py-2 rounded w-[300px] lg:w-[500px]"
           />
 
           <label>Email<span class="text-red-500">*</span></label>
           <input
             type="email"
             placeholder="Type..."
-            class="block text-white bg-black border border-white px-4 py-2 rounded w-[300px]"
+            class="block text-white bg-black border border-white mb-2 px-4 py-2 rounded w-[300px] lg:w-[500px]"
           />
 
           <label>Company<span class="text-red-500">*</span></label>
           <input
             type="text"
             placeholder="Type..."
-            class="block text-white bg-black border border-white px-4 py-2 rounded w-[300px]"
+            class="block text-white bg-black border border-white mb-2 px-4 py-2 rounded w-[300px] lg:w-[500px]"
           />
           <app-button
             label="Submit"
