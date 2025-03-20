6b206a8e82cea94449fb94834633ba336b891f26
Jaspreet <jaspreetkn92@gmail.com>
2025-03-20 12:21:55 +0000

    cleaned and refactored the templates into HTML files

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -5,28 +5,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-product-card',
   imports: [ButtonComponent, RouterModule],
-  template: `
-    <section class="text-white mb-10 lg:hover:bg-red-900 lg:transition-all">
-      <div class="border-t border-white">
-        <h3 class="text-2xl">{{ number }}</h3>
-        <div class="p-6">
-          <h1 class="text-4xl font-semibold mb-12">{{ title }}</h1>
-          <p class="font-light mb-2">{{ description }}</p>
-          <div class="hidden lg:block">
-            <a routerLink="/products">
-              <i class="fa-solid fa-arrow-right text-4xl"></i>
-            </a>
-          </div>
-        </div>
-        <a routerLink="/products">
-          <app-button
-            customClass="bg-red-400 w-full text-left text-white hover:bg-red-300 lg:hidden"
-            label="Find out more"
-          ></app-button>
-        </a>
-      </div>
-    </section>
-  `,
+  templateUrl: './product-card.component.html',
 })
 export class ProductCardComponent {
   @Input() number: number = 0;
--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -6,39 +6,7 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-product-item',
   imports: [ButtonComponent, RouterModule, CommonModule],
-  template: `
-    <section class="text-white p-6">
-      <div class="flex flex-col md:flex-row md:gap-20 md:justify-between">
-        <div>
-          <div class="flex gap-8">
-            <h2 class="text-2xl">{{ number }}</h2>
-            <h1 class="text-4xl font-semibold mb-4">{{ title }}</h1>
-          </div>
-
-          <p
-            *ngIf="isExpanded"
-            [innerHTML]="description"
-            class="px-8 font-light"
-          ></p>
-
-          <a routerLink="/contact" *ngIf="isExpanded">
-            <app-button
-              label="Request a demo"
-              class="text-left px-8"
-              customClass="mt-4"
-            ></app-button>
-          </a>
-        </div>
-
-        <img
-          [src]="image"
-          alt="product image"
-          *ngIf="isExpanded"
-          class="md:w-[600px]"
-        />
-      </div>
-    </section>
-  `,
+  templateUrl: './product-item.component.html',
 })
 export class ProductItemComponent {
   @Input() number: number = 0;
