5f8480e2cd5df7bf454af388e2724d4e842eec25
Jaspreet <jaspreetkn92@gmail.com>
2025-03-20 12:36:38 +0000

    created constants folder and created footer-links

---
+++ src/app/constants/footer-links.ts
@@ -0,0 +1,22 @@
+export const FOOTER_LINKS = [
+  {
+    label: 'Home',
+    route: '/',
+  },
+  {
+    label: 'Product',
+    route: '/products',
+  },
+  {
+    label: 'About us',
+    route: '/about',
+  },
+  {
+    label: 'Blog',
+    route: '/blog',
+  },
+  {
+    label: 'Contact',
+    route: '/contact',
+  },
+];
